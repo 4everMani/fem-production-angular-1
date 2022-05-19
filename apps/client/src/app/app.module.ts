843f9bcce176f1b90e570a2076be94b1f22b67eb
Manish Jaiswal <jaiswal.manish49@gmail.com>
2022-05-19 06:13:40 +0530

    added remaining changes

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreDataModule } from '@fem/core-data';
 import { HttpClientModule } from '@angular/common/http';
+import { UiToolbarModule } from '@fem/ui-toolbar';
+import { MaterialModule } from '@fem/material';
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,6 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     HttpClientModule,
     CoreDataModule,
+    UiToolbarModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent],
